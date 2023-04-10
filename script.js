93d420e2cde6984cb4fc890bbd190965a9e68e5d
Twaha Kibirige <110776212+T-KibCode@users.noreply.github.com>
2023-04-10 01:05:07 +0100

    Security check
    Removing visibility on sensitive information

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "ad2be7f88amshb389857c019ff11p12d386jsn9fa9dc12feb9",
-		"X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
-	}
-};
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
