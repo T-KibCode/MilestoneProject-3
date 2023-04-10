48b3800970c7fe7c7c56832f3bd7ca320823f098
T-KibCode <tkibirige94@gmail.com>
2023-04-10 19:00:33 +0100

    Saving work thus far. Flask implementation whilst following wireframe plan
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+import requests
+
+url = "https://online-movie-database.p.rapidapi.com/auto-complete"
+
+querystring = {"q":"game of thr"}
+
+headers = {
+	"X-RapidAPI-Key": "ad2be7f88amshb389857c019ff11p12d386jsn9fa9dc12feb9",
+	"X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World</p>"
