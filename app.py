a0854e452788e48352c1b1f0b82f3d31d3b3203a
T-KibCode <tkibirige94@gmail.com>
2023-04-10 19:01:10 +0100

    Flask edits
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-import requests
+## import requests
 
-url = "https://online-movie-database.p.rapidapi.com/auto-complete"
+## url = "https://online-movie-database.p.rapidapi.com/auto-complete"
 
-querystring = {"q":"game of thr"}
+## querystring = {"q":"game of thr"}
 
-headers = {
-	"X-RapidAPI-Key": "ad2be7f88amshb389857c019ff11p12d386jsn9fa9dc12feb9",
-	"X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
-}
+## headers = {
+	## "X-RapidAPI-Key": "ad2be7f88amshb389857c019ff11p12d386jsn9fa9dc12feb9",
+	## "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
+## }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+## response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+## print(response.text)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World</p>"
+def index():
+    return "Hello, World"
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT","5000")),
+        debug=True) ##Do not run debug = true when deployed, only when testing ##
+    
 
