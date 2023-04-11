7383726a742691fb74e732683579eed1964a9c60
T-KibCode <tkibirige94@gmail.com>
2023-04-11 15:39:48 +0100

    credentials added

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
-import os
-from flask import Flask
+import requests
 
-app = Flask(__name__)
+url = "https://online-movie-database.p.rapidapi.com/auto-complete"
 
-## import requests
+querystring = {"q":"game of thr"}
 
-## url = "https://online-movie-database.p.rapidapi.com/auto-complete"
+headers = {
+	"X-RapidAPI-Key": "ad2be7f88amshb389857c019ff11p12d386jsn9fa9dc12feb9",
+	"X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
+}
 
-## querystring = {"q":"game of thr"}
+response = requests.request("GET", url, headers=headers, params=querystring)
 
-## headers = {
-	## "X-RapidAPI-Key": "ad2be7f88amshb389857c019ff11p12d386jsn9fa9dc12feb9",
-	## "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
-## }
-
-## response = requests.request("GET", url, headers=headers, params=querystring)
-
-## print(response.text)
-
-@app.route("/")
-def index():
-    return "Hello, World"
-
-if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT","5000")),
-        debug=False) ##Do not run debug = true when deployed, only when testing ##
-    
-
+print(response.text)
