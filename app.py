0e97b9553ed9451fdbf371f7d3f089fa7cc8edfd
T-KibCode <tkibirige94@gmail.com>
2023-04-11 17:17:22 +0100

    Bootstrap installed *
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import requests
+import moviecreds
 
-url = "https://online-movie-database.p.rapidapi.com/auto-complete"
+# https://developers.themoviedb.org/ - Documentation #
+
+url ="https://api.themoviedb.org/3/authentication/token/new?api_key=9ee4a43299a1ad44c8b7db387a6ee72b"
 
 querystring = {"q":"game of thr"}
 
