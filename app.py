05f705ab5e69e76be5deec4d3bd47caed72022b6
T-KibCode <tkibirige94@gmail.com>
2023-04-12 22:13:56 +0100

    trying to fix python template rendering
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -3,17 +3,9 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 @app.route("/<name>")
-def user(name):
-    return f"Hello {name}"
-
-@app.route("/admin/")
-def admin():
-    return redirect(url_for("user",name="Admin!"))
+def home(name):
+    return render_template("index.html", content=name, r=2)
 
 if __name__ == "__name__":
     app.run()
