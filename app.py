bcdbe6dbee914bbb1f72cfaa52c2ce4ab5fbcfae
T-KibCode <tkibirige94@gmail.com>
2023-04-12 21:03:59 +0100

    trying to fix python rile not running within the browser
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "Hello! this is the main page" 
 
 if __name__ == "__name__":
-    app.run()
+    app.run(
+        host=os.environ.get("IP","0.0.0.0"),
+        port=int(os.environ.get("PORT","5000")),
+        debug=True)
+
+
 
