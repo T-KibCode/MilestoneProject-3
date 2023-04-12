f89742e5faaf035a627f4013eb7ca0b506c8c25b
T-KibCode <tkibirige94@gmail.com>
2023-04-12 16:19:37 +0100

    creating base templates for project.
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -2,26 +2,12 @@ import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
-def create_app():
-  app = Flask(__name__)
-  Bootstrap(app)
+app = Flask(__name__)
 
-  return app
+@app.route("/")
+def home():
+    return "Hello! this is the main page" 
 
-# import requests
-# import moviecreds
+if __name__ == "__name__":
+    app.run()
 
-# https://developers.themoviedb.org/ - Documentation #
-
-# url ="https://api.themoviedb.org/3/authentication/token/new?api_key=9ee4a43299a1ad44c8b7db387a6ee72b"
-
-# querystring = {"q":"game of thr"}
-
-#headers = {
-#	"X-RapidAPI-Key": "ad2be7f88amshb389857c019ff11p12d386jsn9fa9dc12feb9",
-#	"X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
-#}
-
-#response = requests.request("GET", url, headers=headers, params=querystring)
-
-#print(response.text)
