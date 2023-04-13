8c15166927107f72bca0401b0798d13b2dccfe07
T-KibCode <tkibirige94@gmail.com>
2023-04-13 21:16:18 +0100

    FINALLY rendered to HTML through python script
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask 
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(
