600bf32afd8b90d219ad0f05506db46389d474d5
T-KibCode <tkibirige94@gmail.com>
2023-04-14 12:13:21 +0100

    FINALLY A STABLE RENDERING OF THE FILE. testing using the python test tools built in to vscode
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask, render_template
-from jinja2 import Environment, PackageLoader, select_autoescape
-env = Environment(
-    loader=PackageLoader("app.py"),
-    autoescape=select_autoescape()
-)
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=name, r=2)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(
-        host=("IP","0.0.0.0"),
-        port=int("PORT","5000"),
-        debug=True
-    )
-
+    app.run(debug=True)
