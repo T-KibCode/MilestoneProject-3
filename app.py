ea60f65a206f198294d1ec754d0e3e0de53cbb73
T-KibCode <tkibirige94@gmail.com>
2023-04-14 15:35:35 +0100

    commiting changes for page url's that are present when page is loaded
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home(name):
+def home():
     return render_template("index.html")
 
 @app.route("/login")
-def login(name):
+def login():
     return render_template("login.html")
 
 @app.route("/search")
-def search(name):
+def search():
     return render_template("search.html")
 
 
