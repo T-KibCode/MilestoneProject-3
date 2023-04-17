eaae01a518d5fd67673750934ffb3122b5c2d832
T-KibCode <tkibirige94@gmail.com>
2023-04-17 21:31:20 +0100

    still trying to figuure out the format of python code relating to get requests from the TMDB search API
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
@@ -6,10 +7,9 @@ app = Flask(__name__)
 def home():
     url = "https://api.themoviedb.org/3/movie/550?api_key=9ee4a43299a1ad44c8b7db387a6ee72b"
     response = requests.get(url)
-    data = response.json()["results"][:3] ## Testing to attempt to limit called responses to 3 title cards for Caroseul
-    for movie in data:
-        movie["poster_path"] = "https://image.tmdb.org/t/p/w500" + movie["poster_path"]
-        movie["vote_average"] = int(movie["vote_average"] * 10) ## Attempt to convert call back answer in to a percentage for visual representation.
+    data = [{"title": response.json()["title"],
+        "poster_path": "https://image.tmdb.org/t/p/w500" + response.json()["poster_path"],
+        "vote_average": int(response.json()["vote_average"] * 10)}]
     return render_template("index.html", movies=data)
 
 @app.route("/login")
