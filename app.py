cdec79f70b21a950f890631a9c90cef408949fce
T-KibCode <tkibirige94@gmail.com>
2023-04-21 23:36:24 +0100

    Have a Firm understanding of Flask now. Slowly working towards User Authentication. Finishing Building out HTML Structure first.
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -4,18 +4,9 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    url = "https://api.themoviedb.org/3/trending/all/day?api_key=9ee4a43299a1ad44c8b7db387a6ee72b"
-    response = requests.get(url)
-    data = []
-    for result in response.json()["results"]:
-        movie = {
-            "title": result.get("title", result.get("name", "Unknown")),
-            "poster_path": "https://image.tmdb.org/t/p/w500" + result.get("poster_path", ""),
-            "vote_average": int(result.get("vote_average", 0) * 10)
-        }
-        data.append(movie)
-    return render_template("index.html", movies=data)
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
@@ -26,6 +17,7 @@ def search():
     return render_template("search.html")
 
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
