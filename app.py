bb5924c11978e4fa653c20ee5f23256b56581aff
T-KibCode <tkibirige94@gmail.com>
2023-04-21 23:37:06 +0100

    Building out HTML structure
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -3,14 +3,33 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+## Dummy Data 
+
+posts = [
+    {
+    'author': 'Twaha Kibirige',
+    'title': 'Blog Post',
+    'content': 'First post content',
+    'date_posted': 'January 1st, 2020',
+    },
+    {
+    'author': 'John Smith',
+    'title': 'Blog Post',
+    'content': 'First post content',
+    'date_posted': 'December 12th, 2021',
+    }
+]
+
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", title="About")
 
 @app.route("/search")
 def search():
@@ -20,4 +39,5 @@ def search():
 if __name__ == "__main__":
     app.run(debug=True)
 
-    
+
+
