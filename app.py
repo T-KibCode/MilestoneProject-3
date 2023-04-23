7a4b5d2e9415d6858b196b0ea9a856db620adbce
T-KibCode <tkibirige94@gmail.com>
2023-04-23 22:08:21 +0100

    <feat> [creating db database instance]
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import requests
+from datetime import datetime
 from flask import Flask, redirect, url_for, render_template, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -7,8 +9,31 @@ app = Flask(__name__)
 
 ## Good practice to secure session token with secret keys
 app.config['SECRET_KEY'] = '79d20b7616702de60f914aabc8b86f78'
+## SQL demo database creation
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    post = db.relationship('Post', backref='author',lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    dat_posted =db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 ## Dummy Data 
 posts = [
