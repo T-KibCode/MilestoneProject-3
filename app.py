86435545e81c90c3bc0b296d3790ee843f132ac2
T-KibCode <tkibirige94@gmail.com>
2023-04-23 17:58:34 +0100

    <feat> User registration/login: <User Auth> [Created login forms for registration and login with secret key for user auth]
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import requests
 from flask import Flask, redirect, url_for, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-## Dummy Data 
 
+## Good practice to secure session token with secret keys
+app.config['SECRET_KEY'] = '79d20b7616702de60f914aabc8b86f78'
+
+
+
+## Dummy Data 
 posts = [
     {
     'author': 'Twaha Kibirige',
@@ -27,17 +33,19 @@ posts = [
 def home():
     return render_template("index.html", posts=posts)
 
-@app.route("/login")
-def login():
-    return render_template("login.html", title="Login")
-
 @app.route("/search")
 def search():
     return render_template("search.html", title="Film Search")
 
 @app.route("/signup")
 def signup():
-    return render_template("signup.html", title="Signup")
+    form = RegistrationForm()
+    return render_template("signup.html", title="Signup", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 
