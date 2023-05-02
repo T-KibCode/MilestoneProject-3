ac816a2b9bb10bd4772c3d8d46e20416652822a7
T-KibCode <tkibirige94@gmail.com>
2023-05-02 13:01:36 +0100

    <fix>[trying to fix broken flask render]
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- flickfanatic/routes.py
+++ flickfanatic/routes.py
@@ -1,10 +1,11 @@
-import requests
-from flask import render_template, url_for, redirect, flash, redirect
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect
 from flickfanatic import app, db, bcrypt
-from flickfanatic.forms import RegistrationForm, LoginForm
+from flickfanatic.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flickfanatic.models import User, Post
-from flask_login import login_user, current_user, logout_user
-
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 ## Dummy Data 
@@ -26,11 +27,11 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html", posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
-@app.route("/search")
-def search():
-    return render_template("search.html", title="Film Search")
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 @app.route("/signup", methods=['GET','POST'])
 def signup():
@@ -44,7 +45,7 @@ def signup():
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
-    return render_template("signup.html", title="Signup", form=form)
+    return render_template('signup.html', title='Signup', form=form)
 
 @app.route("/login", methods=['GET','POST'])
 def login():
@@ -55,17 +56,46 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template("login.html", title="Login", form=form)
+    return render_template('login.html', title='Login', form=form)
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-@app.route("/account")
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',image_file=image_file, form=form)
