5f10200cf01355094c43da505d31168dcf9be639
T-KibCode <tkibirige94@gmail.com>
2023-05-05 10:05:23 +0100

    <feat> update readme <fix>
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

---
+++ flickfanatic/static/styles/script.js
@@ -0,0 +1,17 @@
+    $(document).ready(function() {
+      $(".btn-primary").click(function() {
+        var movie = $(".form-control").val();
+        var category = $(".form-select").val();
+        $.ajax({
+          url: "/search",
+          method: "POST",
+          data: {
+            movie: movie,
+            category: category
+          },
+          success: function(response) {
+            console.log(response);
+          }
+        });
+      });
+    });
