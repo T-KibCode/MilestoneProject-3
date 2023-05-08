eb7c7885c2d53fe9b6bf5a53f485f34573f59b23
T-KibCode <tkibirige94@gmail.com>
2023-05-08 13:42:33 +0100

    <feat> (flickfanatic): add search functionality (building blocks for the search functionality)
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- flickfanatic/static/styles/script.js
+++ flickfanatic/static/styles/script.js
@@ -1,17 +1,23 @@
-    $(document).ready(function() {
-      $(".btn-primary").click(function() {
-        var movie = $(".form-control").val();
-        var category = $(".form-select").val();
-        $.ajax({
-          url: "/search",
-          method: "POST",
-          data: {
-            movie: movie,
-            category: category
-          },
-          success: function(response) {
-            console.log(response);
-          }
-        });
-      });
+/* 
+
+$(document).ready(function () {
+  $(".btn-primary").click(function () {
+    var movie = $(".form-control").val();
+    var category = $(".form-select").val();
+    $.ajax({
+      url: "/search",
+      method: "POST",
+      data: {
+        movie: movie,
+        category: category
+      },
+      success: function (response) {
+        console.log(response);
+      }
     });
+  });
+});
+
+*/
+
+// Query Selectors to call results from movies.session.sql database
