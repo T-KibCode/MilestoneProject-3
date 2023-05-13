cc537c8fccebeb7d4227068c81665199056618c7
T-KibCode <tkibirige94@gmail.com>
2023-05-13 23:48:17 +0100

    <fix> data in sql database + link to remote server
    Signed-off-by: T-KibCode <tkibirige94@gmail.com>

--- admin.session.sql
+++ admin.session.sql
@@ -6,7 +6,38 @@ SELECT * FROM actor;
 SELECT * FROM movie;
 
 -- @BLOCK
-SELECT * FROM actor_movie;
+SELECT * FROM actor_movie (actor_id) VALUES (30);
+
 
 -- @BLOCK
 SELECT * FROM actor_movie WHERE actor_id = 1;
+
+-- @BLOCK
+
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (1, 11) ;
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (21, 8);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (9, 2);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (9, 3);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (30, 2);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (30, 3);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (7, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (8, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (9, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (10, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (11, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (12, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (13, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (14, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (15, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (16, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (17, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (18, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (19, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (20, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (21, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (22, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (23, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (24, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (25, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (26, 1);
+INSERT INTO actor_movie (actor_id, movie_id) VALUES (27, 1);
