f4e3b1197313ed6e803a26d0466ef73f47d90604
Twaha Kibirige <tkibirige94@gmail.com>
2023-05-31 22:52:08 +0100

    Update forms.py
    <fix> PEP8 Syntax phase 2

--- flickfanatic/forms.py
+++ flickfanatic/forms.py
@@ -1,70 +1,93 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, \
+    BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, \
+    ValidationError
 from flickfanatic.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+            validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('That username is taken. Please choose a different one.'
+                                  )
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError('That email is taken. Please choose a different one.'
+                                  )
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png','jpeg','gif','jfif','webp'])])
+
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture',
+                        validators=[FileAllowed([
+        'jpg',
+        'png',
+        'jpeg',
+        'gif',
+        'jfif',
+        'webp',
+        ])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
-        if username.data != current_user.username: # type: ignore
+        if username.data != current_user.username:  # type: ignore
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
+                raise ValidationError('That username is taken. Please choose a different one.'
+                        )
 
     def validate_email(self, email):
-        if email.data != current_user.email: # type: ignore
+        if email.data != current_user.email:  # type: ignore
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
-            
+                raise ValidationError('That email is taken. Please choose a different one.'
+                        )
+
 
 class PostForm(FlaskForm):
+
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
-            
+
+
 class MovieForm(FlaskForm):
-    moviename=StringField('Enter Movie Name...',validators=[DataRequired()])
-    submit=SubmitField('Search')
+
+    moviename = StringField('Enter Movie Name...',
+                            validators=[DataRequired()])
+    submit = SubmitField('Search')
+
 
 class ActorForm(FlaskForm):
-    actorname=StringField('Enter Actor Name...',validators=[DataRequired()])
-    submit=SubmitField('Search')
+
+    actorname = StringField('Enter Actor Name...',
+                            validators=[DataRequired()])
+    submit = SubmitField('Search')
