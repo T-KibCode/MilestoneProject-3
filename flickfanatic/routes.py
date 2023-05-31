feb19037fd18b424833d7b9c98391711d23f0e44
Twaha Kibirige <tkibirige94@gmail.com>
2023-05-31 22:53:47 +0100

    Update routes.py
    <fix> PEP8 Syntax phase 3

--- flickfanatic/routes.py
+++ flickfanatic/routes.py
@@ -1,67 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 from pydoc import pager
 import secrets
 import jwt
 from PIL import Image
-from flask import abort, render_template, url_for, flash, redirect, request
+from flask import abort, render_template, url_for, flash, redirect, \
+    request
 from flickfanatic import app, db, bcrypt
-from flickfanatic.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flickfanatic.forms import RegistrationForm, LoginForm, \
+    UpdateAccountForm, PostForm
 from flickfanatic.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import login_user, current_user, logout_user, \
+    login_required
 
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
-@app.route("/search")
-def search(): 
+
+@app.route('/search')
+def search():
     return render_template('search.html', title='Movie Search')
 
-   
 
-@app.route("/register", methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:  # type: ignore
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = \
+            bcrypt.generate_password_hash(form.password.data).decode('utf-8'
+                )
+        user = User(username=form.username.data, email=form.email.data,
+                    password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
+        flash('Your account has been created! You are now able to log in'
+              , 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login", methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated: # type: ignore
+    if current_user.is_authenticated:  # type: ignore
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password,
+                form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return (redirect(next_page) if next_page else redirect(url_for('home'
+                    )))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password'
+                  , 'danger')
     return render_template('login.html', title='login', form=form)
 
-@app.route("/logout")
+
+@app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
+    (_, f_ext) = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(app.root_path, 'static/profile_pics',
+                                picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
@@ -71,7 +87,7 @@ def save_picture(form_picture):
     return picture_fn
 
 
-@app.route("/account", methods=['GET', 'POST'])
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
@@ -85,17 +101,21 @@ def account():
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
     elif request.method == 'GET':
-        form.username.data = current_user.username # type: ignore
-        form.email.data = current_user.email # type: ignore
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file) # type: ignore
-    return render_template('account.html', title='Account',image_file=image_file, form=form)
+        form.username.data = current_user.username  # type: ignore
+        form.email.data = current_user.email  # type: ignore
+    image_file = url_for('static', filename='profile_pics/'
+                         + current_user.image_file)  # type: ignore
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
 
-@app.route("/post/new", methods=['GET', 'POST'])
+
+@app.route('/post/new', methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data, content=form.content.data,
+                    author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
@@ -104,13 +124,13 @@ def new_post():
                            form=form, legend='New Post')
 
 
-@app.route("/post/<int:post_id>")
+@app.route('/post/<int:post_id>')
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
 
-@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -130,7 +150,7 @@ def update_post(post_id):
                            form=form, legend='Update Post')
 
 
-@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
 @login_required
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
