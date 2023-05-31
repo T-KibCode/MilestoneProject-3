3f5b416daee281f80a5439a8e7d70d9983408a53
Twaha Kibirige <tkibirige94@gmail.com>
2023-05-31 22:50:55 +0100

    Update __init__.py
    <fix> PEP8 Syntax phase 1

--- flickfanatic/__init__.py
+++ flickfanatic/__init__.py
@@ -1,19 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-if os.path.exists("env.py"):
-    import env #noqa
+if os.path.exists('env.py'):
+    import env  # noqa
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tokenSecretKey'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view ='login' # type: ignore
+login_manager.login_view = 'login'  # type: ignore
 login_manager.login_message_category = 'info'
 
-
-from flickfanatic import routes 
+from flickfanatic import routes
